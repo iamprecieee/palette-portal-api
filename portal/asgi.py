b0a9879f6476680e3c0532b2b923d6a41ddb04a4
iamprecieee <emmypresh777@gmail.com>
2024-07-05 20:07:28 +0100

    update to fix app_registry issue

--- portal/asgi.py
+++ portal/asgi.py
@@ -7,15 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
-
+import django
 from django.core.asgi import get_asgi_application
+
+import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
+
+# Setup Django
+django.setup()
 
 from chat.routing import websocket_urlpatterns
 
